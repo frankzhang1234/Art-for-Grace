1f59addad7972b597996f932d7b5601f2df777de
frankzhang1234 <frankz101201@gmail.com>
2026-04-17 00:26:24 -0700

    Update artworks.ts

--- src/data/artworks.ts
+++ src/data/artworks.ts
@@ -22,7 +22,7 @@ export interface Artwork {
 
 const artworks: Artwork[] = [
   {
-    src: "/artworks/Grace Project portait_colored.png",
+    src: "/artworks/Grace Project portrait_colored.png",
     title: "Another Drawing of You",
     date: "April 17, 2026",
     description: "This actually took a while and it's part of a project I'm making for you :)",
